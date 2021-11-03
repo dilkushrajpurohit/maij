529968fc7c7668e664b124d43962a6131e15d328
dilkushrajpurohit <dilkushpurohit963@gmail.com>
2021-11-02 23:57:16 -0700

    f

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const Article=require("./models/article")
 const articlerouter=require('./routes/articles')
 const mongnoose=require("mongoose")
 app.use(express.urlencoded({extended:false}))
-mongnoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/blog',{useNewUrlParser:true, useUnifiedTopology:true})
 app.set('view engine','ejs')
 app.use('/articles',articlerouter)
 
 const port=process.env.PORT||400
-
+mongoose
+     .connect( process.env.MONGODB_URI || 'mongodb://localhost/blog', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+     .then(() => console.log( 'Database Connected' ))
+     .catch(err => console.log( err ));
 
 app.get("/",async(req,res)=>{
 	const articles=await Article.find().sort({createdAt:'desc'})
